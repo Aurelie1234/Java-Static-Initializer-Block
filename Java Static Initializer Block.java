a5fe2646f641e29341f6d6f464f09ac6d4e6f052
Aurelie1234 <115810657+Aurelie1234@users.noreply.github.com>
2023-06-05 16:25:04 +0000

    Create Java Static Initializer Block.java

---
+++ Java Static Initializer Block.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Solution {
+
+
+public static void main(String[] args) {
+/* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+Scanner scan = new Scanner(System.in);
+int B = scan.nextInt();
+int H = scan.nextInt();
+scan.close();
+
+
+if (B > 0 && H > 0) {
+System.out.println(B*H);
+}
+else { System.out.println("java.lang.Exception: Breadth and height must be positive" );}
+}
+}
